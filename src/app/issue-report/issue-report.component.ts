9d316a6de4c0e20d2f9aa33f82e0fa6161e5a80c
hamzahouri <hamzahouri@gmail.com>
2022-10-03 22:14:35 +0100

    adding validation

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {  FormBuilder, FormGroup } from '@angular/forms'; 
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms'; 
 import { IssuesService } from '../issues.service';
 @Component({
   selector: 'app-issue-report',
@@ -16,15 +16,19 @@ export class IssueReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title : [''],
+      title : ['',Validators.required],
       description : [''],
-      priority : [''],
-      type : ['']
+      priority : ['',Validators.required],
+      type : ['',Validators.required]
 
     });
   }
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+      }
     this.issueService.createIssue(this.issueForm?.value);
     this.formClose.emit();
     }
